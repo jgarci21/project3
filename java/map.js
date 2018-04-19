9958132646dc775effd745aeb7b7394732b1e34f
jgarci21 <jgarci21@hawk.iit.edu>
2018-04-19 00:59:24 -0500

    I have set the basics to test out the map a drop pinpoint. It does not seem to display the map for now.

--- java/map.js
+++ java/map.js
@@ -1,20 +1,15 @@
-<!DOCTYPE html>
-<html>
-  <body>
-    <h1>Map</h1>
-    <div id="map" style="width:400px;height:400px;background:yellow"></div>
-    <script>
-      function myMap() {
-      var mapOptions = {
-        center: new google.maps.LatLng(30.2574624,-97.7436018),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-                       }
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-                       }
-    </script>
-    <script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCAh35vpbs9whGeqzT_4k9Joj3PelnvBqc&callback=myMap">
-    </script>
-</body>
-</html>
+var map;
+var tag;
+var place;
+var chicago={lat: 41.8338192, lng: -87.712234}
+var place=["chicago"];
+function gMap() {
+  place={lat: 41.8338192, lng: -87.712234};
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom:10,
+  center: chicago});
+  tag = new google.maps.Marker({
+    position:chicago,
+    animation:google.maps.Animation.DROP,
+  map: map });
+}
